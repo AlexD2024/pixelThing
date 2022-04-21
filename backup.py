3d20f5a3ab037114f09acd76a7d87ce505ce6166
Alex Dooley <70412849+AlexD2024@users.noreply.github.com>
2022-04-20 21:25:38 -0400

    Closes #3
    Changed backup system to work with the database.

--- backup.py
+++ backup.py
@@ -1,38 +1,21 @@
-#Runs once a day, every day
-import os
-from datetime import date as d
+import mysql.connector
+import sys
 
-colorPathBack = f'/home/CashDot/Pixel Site/backups/{d.today().year}/{d.today()}-col.txt'
-locPathBack = f'/home/CashDot/Pixel Site/backups/{d.today().year}/{d.today()}-loc.txt'
-colPath = '/home/CashDot/Pixel Site/saves/colors.txt'
-locPath = '/home/CashDot/Pixel Site/saves/location.txt'
+#Connects to MYSQL
 
-if not os.path.isdir(f'/home/CashDot/Pixel Site/backups/{d.today().year}'):
-    os.mkdir(f'/home/CashDot/Pixel Site/backups/{d.today().year}')
+db = mysql.connector.connect(
+  host="AlexDooley.mysql.pythonanywhere-services.com",
+  user="AlexDooley",
+  password="Dot($$$)06",
+  database='AlexDooley$PixelCL'
+)
 
-with open(colPath, 'r+') as file:
-    with open(locPath, 'r+') as f:
-        with open(locPathBack, 'w') as locSave:
-            with open(colorPathBack, 'w') as colSave:
-                for item in file.readlines():
-                    colSave.write(item)
-                for item in f.readlines():
-                    locSave.write(item)
+cursor = db.cursor()
 
-#Checks if a new year ticked over.
-if not os.path.isdir(f'/home/CashDot/Pixel Site/saves/previous/{d.today().year - 1}'):
-    os.mkdir(f'/home/CashDot/Pixel Site/saves/previous/{d.today().year - 1}')
-    with open(colPath, 'r+') as file:
-        with open(locPath, 'r+') as f:
-            locations = f.readlines()
-            colors = file.readlines()
-            f.seek(0)
-            file.seek(0)
-            f.truncate(0)
-            file.truncate(0)
-            with open(f'/home/CashDot/Pixel Site/saves/previous/{d.today().year - 1}/colors.txt', 'w') as colSaving:
-                for item in colors:
-                    colSaving.write(item)
-            with open(f'/home/CashDot/Pixel Site/saves/previous/{d.today().year - 1}/location.txt', 'w') as locSaving:
-                for item in locations:
-                    locSaving.write(item)
+def restorePrevious(length):
+    cursor.execute(f'DELETE FROM pixels WHERE date > DATE(NOW() - INTERVAL {length} DAY)')
+    db.commit()
+    print('deleted!')
+
+if __name__ == '__main__':
+    restorePrevious(sys.argv[1])
